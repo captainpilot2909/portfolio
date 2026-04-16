3ebdcd4c69a217e6504c75f2f7dae08ef100a887
Riyan Ahmed <entrepreneur2909@gmail.com>
2026-04-16 17:56:57 +0500

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,20 @@
+print("""Welcome!
+      This is Riyan Ahmad khan 
+      from today i have started 
+      a Daily habbit.
+    MISSION:    (I will code one program no 
+                matter how small or large it is
+                everyday.
+                MIN= 10 LINES OF PROPER CODE(HAVING SOME MEANING)
+                MAX= NOT MORE THAN 30 MINS (IF DO SO PENALTY (NO SHOPPING FOR 7 DAYS))
+                )
+    TARGET:     Understand and explain the concept you learn to switch board
+    CONSTRAINT: EVERY DAY LEARN NEW THING EVEN /n 
+    TIMELINE:   ATLEAT TILL GRADUATION
+    PANALTY:    IF SKIP 1 DAY HAVE TO CONFESS YOUR THOUGHTS TO SOME FRIEND
+    
+    THATS ALL FOR FIRST DAY THE DEAL IS SEAL AND SHUT DOWN 
+    (ALLAH ISTQAMAT ATTA FARMAIY AMEEN(INSHALLAH))
+      """)
+
+print("HELLO WORLD BE PREPARED FOR RIYAN THE EMPEROR")
